c6017a848dc95daec8411c359779a6c3a9b7ea5f
Zhanno <159880480+Zhanno@users.noreply.github.com>
2024-05-30 18:31:27 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,3 @@
+cos = input()
+pos = input()
+print ("Current location is", cos,("and time is"),pos)
---
+++ 3.py
@@ -0,0 +1,9 @@
+g1 = input()
+g2 = input()
+g3 = ' '
+while g3 !="":
+    g3 = input ()
+if g1 <= g3 >= g2:
+    print (True)
+else:
+    print (False)
